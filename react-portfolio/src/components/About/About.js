a1c52b14b963b790f204feac5112a550a3292330
BogartDME <edwin.pietrowski@gmail.com>
2023-03-29 12:23:16 -0400

    working on drop downs in About.js About Me drop down works, skills drop down not working yet

--- react-portfolio/src/components/About/About.js
+++ react-portfolio/src/components/About/About.js
@@ -1,28 +1,49 @@
-import React from "react";
+
+import React,{useState} from 'react';
 import RLcar from "../../assets/images/RLcar.PNG"
 
-
 export default function About() {
+    const [readMore1,setReadMore1]=useState(false);
+    const [readMore2,setReadMore2]=useState(false);
+    
+    const extraContent1=<container className="flex flex-col items-center">
+        <div className='w-3/4 space-y-16 flex flex-col items-center bg-gradient-to-r from-blue to-darkGreen rounded-lg'>
+        <p className="extra-content1 text-2xl w-3/4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur nequeab 
+            porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero 
+            commodi officia aliquam! Maxime.
+        </p>
+        </div>
+    </container>
+    const aboutMe=<div className='flex flex-col items-center text-4xl pt-20'>
+        About Me
+    </div>
+
+    const extraContent2=<container className="flex flex-col items-center">
+    <div className='w-3/4 space-y-16 flex flex-col items-center bg-gradient-to-r from-blue to-darkGreen rounded-lg'>
+    <p className="extra-content2 text-2xl w-3/4">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, consectetur nequeab 
+        porro quasi culpa nulla rerum quis minus voluptatibus sed hic ad quo sint, libero 
+        commodi officia aliquam! Maxime.
+    </p>
+    </div>
+    </container>
+    const skills=<div className='flex flex-col items-center text-4xl pt-20'>
+    Skills
+    </div>
+
     return (
         <container className="space-y-16 flex flex-col items-center">
-            <div className="space-y-16">
-                <h1 className="text-5xl">
-                    About Me
-                </h1>
-                <p className="text-2xl">
-                    Text about me
-                </p>
+            <div className="">
+                <a className="read-more-link space-y-12" onClick={()=>{setReadMore1(!readMore1)}}><h2>{aboutMe}</h2></a>
+                {readMore1 && extraContent1}
             </div>
     
                 <img src= {RLcar} alt="picture of me" className="meAbout justify-self-end h-80 w-60"/>
             
-            <div className="space-y-16">
-                <h1 className="text-5xl">
-                    Skills
-                </h1>
-                <p className="text-2xl">
-                    List of skills
-                </p>
+            <div className="">
+                <a className="read-more-link space-y-12" onClick={()=>{setReadMore2(!readMore2)}}><h2>{skills}</h2></a>
+                {readMore2 && extraContent2}
             </div>
         </container>
         
