2c9567edf5f38559a6751203d8d3e88d29da8491
BogartDME <edwin.pietrowski@gmail.com>
2023-03-30 01:42:59 -0400

    styling for skills and about me done

--- react-portfolio/src/components/About/About.js
+++ react-portfolio/src/components/About/About.js
@@ -16,9 +16,14 @@ export default function About() {
         </p>
         </div>
     </container>
-    const aboutMe=<div className='flex flex-col items-center text-4xl pt-20'>
-        About Me
-    </div>
+
+   
+        const aboutMe= <div className=' w-96 bg-gradient-to-b from-gunMetal via-ashGray to-gunMetal rounded-full cursor-pointer flex flex-col items-center text-4xl py-10'>
+            <div>
+                About Me
+            </div>
+        </div>
+    
 
     const extraContent2=<container className="flex flex-col items-center">
     <div className='w-3/4 space-y-16 flex flex-col items-center bg-gradient-to-r from-blue to-darkGreen rounded-lg'>
@@ -29,13 +34,13 @@ export default function About() {
     </p>
     </div>
     </container>
-    const skills=<div className='flex flex-col items-center text-4xl pt-20'>
+    const skills=<div className=' w-96 bg-gradient-to-b from-gunMetal via-ashGray to-gunMetal rounded-full cursor-pointer flex flex-col items-center text-4xl py-10'>
     Skills
     </div>
 
     return (
         <container className="space-y-16 flex flex-col items-center">
-            <div className="">
+            <div className="container-mx-auto flex flex-col items-center">
                 <a className="read-more-link space-y-12" onClick={()=>{setReadMore1(!readMore1)}}><h2>{aboutMe}</h2></a>
                 {readMore1 && extraContent1}
             </div>
@@ -53,7 +58,7 @@ export default function About() {
                 <img src= {RLcar} alt="picture of me" className="meAbout justify-self-end h-80 w-60"/>
             
             <div className="">
-                <a className="read-more-link space-y-12" onClick={()=>{setReadMore2(!readMore2)}}><h2>{skills}</h2></a>
+                <a className="container-mx-auto flex flex-col items-center read-more-link space-y-12" onClick={()=>{setReadMore2(!readMore2)}}><h2>{skills}</h2></a>
                 {readMore2 && extraContent2}
             </div>
         </container>
